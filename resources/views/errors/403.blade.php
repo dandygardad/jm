ff2fad4df7857b4f706827fa05b08677443a2fa7
dandygardad <dandygardad@gmail.com>
2021-10-18 17:12:28 +0800

    Add SCSS from Toko

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Error 403</title>
+    <link rel="stylesheet" href="css/error.css">
+</head>
+<body>
+    <div class="container">
+        <div class="text">
+            <h1>Error 403</h1>
+            <p>Tidak diizinkan</p>
+            <a href="/"><img src="assets/img/jm-logo.png" alt="Logo Jaya Mandiri"></a>
+        </div>
+    </div>
+</body>
+</html>
