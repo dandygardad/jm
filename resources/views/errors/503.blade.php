c29a3347876975a522341ccd4ef7e4698cd4ce4b
dandygardad <dandygardad@gmail.com>
2021-10-24 15:55:50 +0800

    Update error 503

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Error 403</title>
+    <title>Maintenance</title>
     <link rel="stylesheet" href="{{ url('/css/error.css') }}">
 </head>
 <body>
@@ -15,6 +15,7 @@
             <div class="img">
                 <a href="/"><img src="{{ url('/assets/img/jm-logo.png') }}" alt="Logo Jaya Mandiri"></a>
             </div>
+
         </div>
     </div>
 </body>
